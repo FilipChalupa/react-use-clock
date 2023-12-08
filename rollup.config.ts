e0bcd0e14e2152f64a19b50b1abd1e39a2ae2c8d
Filip Chalupa <chalupa.filip@gmail.com>
2023-12-08 13:55:25 +0100

    Update rollup

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,10 +2,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import typescript from 'rollup-plugin-typescript2'
-//import packageJson from './package.json'
-import fs from 'fs'
-
-const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf-8'))
+import packageJson from './package.json' assert { type: 'json' }
 
 export default {
 	input: './src/index.tsx',
